c87f9ba332c05535d5a3ce15f355cf8f5d81bf20
Michal Janiec <michal.janiec95@gmail.com>
2020-07-22 21:45:48 +0200

    Updates

---
+++ precache-manifest.dcc43eb93939affa01f4c0882880bc43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54e0f2f07f84cb9bd2c2ef3880347e6",
+    "url": "/toDO-list-react/index.html"
+  },
+  {
+    "revision": "d767afdc5aa445d14701",
+    "url": "/toDO-list-react/static/css/main.7723445a.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/toDO-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDO-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d767afdc5aa445d14701",
+    "url": "/toDO-list-react/static/js/main.55c35e21.chunk.js"
+  },
+  {
+    "revision": "6d6c470f354e3fdc8e42",
+    "url": "/toDO-list-react/static/js/runtime-main.aa0dc49e.js"
+  },
+  {
+    "revision": "9f2b8c116b0e9a7ab9387ec7c9a42b1f",
+    "url": "/toDO-list-react/static/media/checkerboard-cross.9f2b8c11.png"
+  }
+]);

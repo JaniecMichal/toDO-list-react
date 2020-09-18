9773771c4768fc1cfb2d953eda0598ede07bedeb
Michal Janiec <michal.janiec95@gmail.com>
2020-09-19 00:37:51 +0200

    Create saga for fetching example tasks and connect saga to store

--- src/App/features/tasks/store.js
+++ src/App/features/tasks/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddelware from "redux-saga";
+import { watchExampleTasks } from "./taskSaga";
 import tasksReducers from "./taskSlice";
 
-export default configureStore({
+const sagaMiddelware = createSagaMiddelware();
+
+const store = configureStore({
     reducer: {
         tasks: tasksReducers,
     },
+    middleware: [sagaMiddelware]
 });
+
+sagaMiddelware.run(watchExampleTasks);
+
+export default store;

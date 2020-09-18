2c594d73bc39f7a06acfaddb58d6acb6abfc2047
Michal Janiec <michal.janiec95@gmail.com>
2020-09-19 00:13:10 +0200

    Assign fetchExampleTasks to button for download example tasks

--- src/App/features/tasks/Tasks.js
+++ src/App/features/tasks/Tasks.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from "react-redux";
+import { fetchExampleTasks } from "./taskSlice";
 import Form from "./Form";
 import TaskOfList from "./TaskOfList";
 import ButtonsUltimateFeatures from "./ButtonsUltimateFeatures";
@@ -10,6 +12,8 @@ import { Button } from "../tasks/Button";
 
 function Tasks() {
 
+  const dispatch = useDispatch();
+
   return (
     <Container>
       <Header mainTitle="React ToDO list by {Imperator}" />
@@ -17,7 +21,7 @@ function Tasks() {
       <Section
         title="Dodaj nowe zadanie"
         body={<Form />}
-        headerExtraContent={<Button>Pobierz przykładowe zadania</Button>}
+        headerExtraContent={<Button onClick={() => dispatch(fetchExampleTasks())}>Pobierz przykładowe zadania</Button>}
       />
 
       <Section

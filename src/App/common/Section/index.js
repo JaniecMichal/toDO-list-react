b8f675562ef3192506700b200ca095723adaa919
Michal Janiec <michal.janiec95@gmail.com>
2020-09-18 23:56:06 +0200

    Add extraHeaderContent property for SectionHeader

--- src/App/common/Section/index.js
+++ src/App/common/Section/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { SectionWrapper, SectionHeader, SectionTitle } from "./styled";
 
-const Section = ({ title, body, extraContent }) => (
+const Section = ({ title, body, extraContent, headerExtraContent }) => (
     <SectionWrapper>
 
         <SectionHeader>
             <SectionTitle>{title}</SectionTitle>
-
+            <div>{headerExtraContent}</div>
         </SectionHeader>
         <div>
             {extraContent}

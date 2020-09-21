6f5b11f71488bb966af1724f0443cb9167262db2
Michal Janiec <michal.janiec95@gmail.com>
2020-09-21 23:05:53 +0200

    Add react-router element with nav

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,34 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, NavLink, Redirect } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage";
+import AuthorPage from "./features/tasks/AuthorPage";
 
-const App = () => <Tasks />;
-
-export default App
+export default () => (
+    <HashRouter>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/zadania">
+                        Menadżer zadań
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/autor">
+                        O autorze
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+        <Switch>
+            <Route path="/zadania">
+                <TasksPage />
+            </Route>
+            <Route path="/autor">
+                <AuthorPage />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
+        </Switch>
+    </HashRouter>
+);

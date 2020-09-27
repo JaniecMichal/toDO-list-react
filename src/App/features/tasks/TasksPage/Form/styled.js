08e4214acec45bfe78d7b2c16afba5f56cceda16
Michal Janiec <michal.janiec95@gmail.com>
2020-09-27 15:37:04 +0200

    Theme references added

--- src/App/features/tasks/TasksPage/Form/styled.js
+++ src/App/features/tasks/TasksPage/Form/styled.js
@@ -6,7 +6,7 @@ export const AddingTaskSection = styled.form`
     justify-content: center;
     align-items: center;
 
-    @media (max-width:790px){
+    @media (max-width:${({ theme }) => theme.breakpoint.mobileMax}px) {
         display: flex;
         flex-flow: column;
         justify-content: center;
@@ -18,21 +18,21 @@ export const AddTaskButton = styled.button`
     margin-left: 5px;
     flex-basis: 20%;
     flex-grow: 1;
-    background-color: teal;
+    background-color:  ${({ theme }) => theme.color.teal};
     color: white;
     padding: 10px;
     border: 2px solid #555;
     transition: 0.5s;
 
     &:hover{
-    background-color: hsl(180, 100%, 33%);
-    color: black;
+    background-color:  ${({ theme }) => theme.color.lightTeal};
+    color:  ${({ theme }) => theme.color.black};
     cursor: pointer;
-    border-color: #222;
+    border-color:  ${({ theme }) => theme.color.mineShaft};
     transform: scale(1.12);
     }
 
-    @media (max-width:790px){
+    @media (max-width:${({ theme }) => theme.breakpoint.mobileMax}px) {
         width: 100%;
         margin-top: 10px;
         margin-left:0px;
@@ -40,7 +40,7 @@ export const AddTaskButton = styled.button`
         &:hover{
             background-color: teal;
             color: white;
-            border-color: #222;
+            border-color:  ${({ theme }) => theme.color.mineShaft};
             transform: none;
             transition:none;
         }
